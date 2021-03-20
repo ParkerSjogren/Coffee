7d67c4fc932b7a643e643d9059a9e7194f8bcb0b
ParkerSjogren <79682858+ParkerSjogren@users.noreply.github.com>
2021-03-19 18:53:00 -0600

    Add files via upload

--- coffee.js
+++ coffee.js
@@ -2,7 +2,6 @@ const btn  = document.querySelector('#btn');
 const btn2 = document.querySelector('#btn2');
 const btn3 = document.querySelector('#btn3');
 
-let selectedValue;
 // handle click button
 
 btn.onclick = function () {
@@ -21,44 +20,47 @@ btn.onclick = function () {
         radioBtn.remove();
         radioBtn2.style.display='block';
         radioBtn2.style.visibility='visible';
+    }
+}
 
-        btn2.onclick = function () {
-            const rbs = document.querySelectorAll('input[name="choice"]');
-            for (const rb of rbs) {
-                if (rb.checked) {
-                    selectedValue = rb.value;
-                    break;
-                }
-            }
-
-            if(selectedValue==='yes'){
-                alert("Dress appropriately")
-            }
-
-            alert("Walk to mailbox retrieve mail")
-            radioBtn2.remove();
-            radioBtn3.style.display='block';
-            radioBtn3.style.visibility='visible';
-
-            btn3.onclick = function () {
-                const rbs = document.querySelectorAll('input[name="choice"]');
-                for (const rb of rbs) {
-                    if (rb.checked) {
-                        selectedValue = rb.value;
-                        break;
-                    }
-                }
-
-                if(selectedValue==='yes'){
-                    alert("Drink coffee & read mail")
-                }
-                else{
-                    alert("Drink coffee")
-                }
-                radioBtn3.remove();
-            }
-
+btn2.onclick = function () {
+    const rbs = document.querySelectorAll('input[name="choice"]');
+    for (const rb of rbs) {
+        if (rb.checked) {
+            selectedValue = rb.value;
+            break;
         }
     }
-   
+
+    if(selectedValue==='yes'){
+        alert("Dress appropriately")
+    }
+    
+    alert("Walk to mailbox retrieve mail")
+    radioBtn2.remove();
+    radioBtn3.style.display='block';
+    radioBtn3.style.visibility='visible';
 }
+
+btn3.onclick = function () {
+    const rbs = document.querySelectorAll('input[name="choice"]');
+    for (const rb of rbs) {
+        if (rb.checked) {
+            selectedValue = rb.value;
+            break;
+        }
+    }
+
+    if(selectedValue==='yes'){
+        alert("Drink coffee & read mail")
+    }
+    else{
+        alert("Drink coffee")
+    }
+    radioBtn3.remove();
+}
+
+        
+    
+   
+
